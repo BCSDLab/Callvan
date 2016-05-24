0e4ce8971b7d6d2d5a84a7a1037d031bc920fd0d
JoEunBee <joeunbi0724@gmail.com>
2016-05-24 14:51:07 +0900

    IntroActivity

---
+++ CallVan/app/src/main/java/com/example/bcsd/callvan/IntroActivity.java
@@ -0,0 +1,39 @@
+package com.example.bcsd.callvan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+/**
+ * Created by EUNBEE on 2016-05-24.
+ */
+public class IntroActivity extends Activity{
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.intro_lin);
+        handler = new Handler();
+        handler.postDelayed(mrun, 3000);
+    }
+
+    Runnable mrun  = new Runnable(){
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    //Intro중에 뒤로가기를 누를 경우 핸들러를 끊음
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(mrun);
+    }
+}
