a3ab24b2ca376f6db6d52b708ca283098e3c3001
JoEunBee <joeunbi0724@gmail.com>
2016-05-26 09:36:56 +0900

    registerActivity

--- CallVan/app/src/main/java/com/example/bcsd/callvan/LoginActivity.java
+++ CallVan/app/src/main/java/com/example/bcsd/callvan/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class LoginActivity extends Activity implements View.OnClickListener{
 
     private TextView login_tv;
+    private TextView regist_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +20,21 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 
         //init login textView
         login_tv = (TextView)findViewById(R.id.login_tv);
+        regist_tv = (TextView)findViewById(R.id.regist_tv);
         login_tv.setOnClickListener(this);
+        regist_tv.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login_tv :
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                Intent log_intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(log_intent);
+                finish();
+            case R.id.regist_tv :
+                Intent res_intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(res_intent);
                 finish();
         }
     }
